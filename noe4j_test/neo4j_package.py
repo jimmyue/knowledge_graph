622e5fb83f4b60be1230d92844ef80536af80f79
Jimmy Yue <53287360+jimmyue@users.noreply.github.com>
2020-09-02 23:07:14 +0800

    Update neo4j_package.py

--- noe4j_test/neo4j_package.py
+++ noe4j_test/neo4j_package.py
@@ -8,7 +8,7 @@ from py2neo import Node,Relationship,Graph,Subgraph,NodeMatcher,RelationshipMatc
 import pandas as pd
 
 class go_neo4j:
-	def __init__(self,host='http://10.10.10.71:17474',username='neo4j',password='abc.123'):
+	def __init__(self,host='http://10.10.10.71:7474',username='neo4j',password='neo4j'):
 		#连接neo4j
 		self.graph = Graph(host, username=username, password=password)
 		#初始化neo4j，清空数据
